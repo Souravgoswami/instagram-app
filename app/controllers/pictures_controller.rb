a1efd32ee274e180c27decec85c044b194cacf79
Sourav Goswami <souravgoswami@protonmail.com>
2020-09-15 05:26:25 +0530

    Show and Index pages

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,5 +1,8 @@
 class PicturesController < ApplicationController
+	before_action :set_pic, only: %i(show edit update destroy)
+
 	def index
+		@pics = Picture.all
 	end
 
 	def new
@@ -7,10 +10,24 @@ class PicturesController < ApplicationController
 	end
 
 	def create
+		@pic = Picture.new(pic_params)
+
+		if @pic.save
+			redirect_to @pic, notice: 'Your picture was posted'
+		else
+			render 'new'
+		end
+	end
+
+	def show
 	end
 
 	private
+	def set_pic
+		@pic = Picture.find(params[:id])
+	end
+
 	def pic_params
-		params.require(:pic).permit(:title, :description)
+		params.require(:picture).permit(:title, :description)
 	end
 end
